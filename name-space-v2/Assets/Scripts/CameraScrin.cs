a60e1eb548146d974e5fc1c257fe4a88474cfe69
Mamory <32417036+Drift2020@users.noreply.github.com>
2017-11-09 09:37:31 +0200

    ferst

---
+++ name-space-v2/Assets/Scripts/CameraScrin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScrin : MonoBehaviour {
+
+    public GameObject pl;
+
+    private void Start()
+    {
+        
+        Screen.autorotateToLandscapeLeft = true;
+        Screen.autorotateToLandscapeRight = true;
+    }
+    // Update is called once per frame
+    void Update () {
+        transform.position = new Vector3(pl.transform.position.x, pl.transform.position.y, -10f);
+
+	}
+}
